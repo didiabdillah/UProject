6e21c251979178a87261fcc5e93fe0d1ba43ac2c
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-12-04 14:49:27 +0700

    Build Home User Interface Frame

--- resources/views/layout/user_main.blade.php
+++ resources/views/layout/user_main.blade.php
@@ -28,19 +28,11 @@
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
+                <li class="nav-item"></li>
+                <li class="nav-item">
+                    <h4 class="mt-1 ml-3">@yield('title')</h4>
+                </li>
             </ul>
-
-            <!-- SEARCH FORM -->
-            <form class="form-inline ml-3">
-                <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
 
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
@@ -113,7 +105,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{route('project')}}" class="nav-link @if(Request::segment(1) == 'project') {{'active'}} @endif">
-                                <i class="nav-icon fas fa-hard-hat"></i>
+                                <i class="nav-icon fas fa-project-diagram"></i>
                                 <p>
                                     Project
                                 </p>
